ef2c93c2fe474e35a32858d0685cc34e7818b2a9
Kamensky Andrey <104049470+Kamensky124@users.noreply.github.com>
2023-01-06 23:08:00 +0700

    Updates

---
+++ precache-manifest.c8fbf009563013d45a926425967780a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc90ae18341285ef4f2c011025c5354",
+    "url": "/IIHWIgnat/index.html"
+  },
+  {
+    "revision": "6f3f41ad93aff95ecf30",
+    "url": "/IIHWIgnat/static/css/main.63355d44.chunk.css"
+  },
+  {
+    "revision": "34ca45fe1a7b210a964d",
+    "url": "/IIHWIgnat/static/js/2.50f95b4a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/IIHWIgnat/static/js/2.50f95b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3f41ad93aff95ecf30",
+    "url": "/IIHWIgnat/static/js/main.29b95b0e.chunk.js"
+  },
+  {
+    "revision": "25bb22000fa8c48654f0",
+    "url": "/IIHWIgnat/static/js/runtime-main.3ce4f230.js"
+  }
+]);
